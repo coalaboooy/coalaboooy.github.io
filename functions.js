8e6876d8204fe3b034df21f1e4dcf2fa880bbcc7
Alex <k.a.s.reporter@gmail.com>
2026-04-26 17:03:21 +0300

    auto push

--- functions.js
+++ functions.js
@@ -1,7 +1,16 @@
+const textBlockClass = "text-block"
+const fontBigClass = "font-big"
+const fontNormalClass = "font-normal"
+
 document.addEventListener("DOMContentLoaded", (event) => {
     const mainContentNode = document.getElementById("mainContentNode");
     const startQuizButton = document.getElementById("startQuizButton");
     startQuizButton.addEventListener("click", function() {
-        mainContentNode.textContent = ""
+        const questionNode = document.createElement("div")
+        questionNode.className = "quiz-content"
+        const questionCounterText = document.createTextNode("Question 67/80")
+        questionCounterText.className = textBlockClass + " " + fontBigClass
+        questionNode.appendChild(questionCounterText)
+        mainContentNode.innerHTML = questionNode
     });
 });
