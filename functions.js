29aaddec93696d5ac4c57c0ac3070d3aa35d5f50
Alex <k.a.s.reporter@gmail.com>
2026-05-01 16:27:33 +0300

    auto push at 2026-05-01 16:27:33

--- functions.js
+++ functions.js
@@ -8,7 +8,7 @@ document.addEventListener("DOMContentLoaded", (event) => {
     $('#umaName').select2({
         placeholder: '',
         data: ["", "Maruzensky", "Nice Nature", "Biwa Hayahide"],
-        width: 'style',
+        width: 'stretch',
         dropdownAutoWidth: true
     });
     const mainContentNode = document.getElementById("mainContentNode");
