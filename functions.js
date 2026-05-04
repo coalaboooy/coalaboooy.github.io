3c2c4473cacafb1ceca27d39eff53fc301a03c77
Alex <k.a.s.reporter@gmail.com>
2026-05-04 12:36:01 +0300

    auto push at 2026-05-04 12:36:01

--- functions.js
+++ functions.js
@@ -1,11 +1,10 @@
-function noMobileAllowed() {
+function noMobileAllowed(mainContentNode) {
     if (navigator.maxTouchPoints > 1) {
-        console.log("tough luck, buddy")
-        const body = document.body;
         const nope = document.createElement("p")
         nope.textContent = "I ain't optimising this shit for mobile, go to the internet cafe or smth"
-        body.innerHTML = ""
-        body.appendChild(nope)
+        nope.className = textBlockClass + " " + fontBigClass
+        mainContentNode.innerHTML = ""
+        mainContentNode.appendChild(nope)
         throw new Error("nuh-uh");
     }
 }
@@ -58,8 +57,8 @@ var count = 1
 var recordedAnswers = []
 
 document.addEventListener("DOMContentLoaded", (event) => {
-    noMobileAllowed()
     const mainContentNode = document.getElementById("mainContentNode")
+    noMobileAllowed(mainContentNode)
     const startQuizButton = document.getElementById("startQuizButton")
     const umaNumP = document.getElementById("umaNum")
     umaNumP.textContent = `There are currently ${audioNum} umamusume characters featured`
